15ccfd72ac6db1da83247d88ff49bd08a27d957c
Mar1Art1S <Oleg7471@outlook.com>
2024-08-29 19:20:56 +0300

    divelop

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import { defineConfig } from "vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: ["resources/js/app.js", "resources/css/app.css"],
             refresh: true,
         }),
-        tailwindcss(),
+
     ],
 
     server: {
